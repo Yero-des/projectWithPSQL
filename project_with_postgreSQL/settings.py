4e50a53cfe94d234579b839e201dfdcdfc3790f4
Yero-des <yero_zc@hotmail.com>
2024-01-07 12:01:20 -0500

    No corsheaders

--- project_with_postgreSQL/settings.py
+++ project_with_postgreSQL/settings.py
@@ -28,11 +28,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = [
-    "http://3.83.93.151/"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://3.83.93.151/"
+# ]
 
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'project_with_postgreSQL.urls'
