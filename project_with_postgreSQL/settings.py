231afde936c09fef513a1332899d5a28fcdddf81
Debian <bitnami@ip-172-26-0-123.ec2.internal>
2024-01-07 17:23:55 +0000

    ?

--- project_with_postgreSQL/settings.py
+++ project_with_postgreSQL/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ngtw*t%rxajhd-m#)ppm6uv=^z81jz9z037ze&6e*krs+%zx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'project_with_postgreSQL.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'people',
+        'NAME': 'database_psql',
         'USER': 'postgres',
-        'PASSWORD': 'Align2312',
+        'PASSWORD': 'L/BUA7C2=juz',
         'HOST': 'localhost',  
         'PORT': '5432',   
     }
