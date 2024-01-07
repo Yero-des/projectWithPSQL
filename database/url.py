ec6fd6166b92e663b4934f0f448b79808ca87cae
Yero-des <yero_zc@hotmail.com>
2024-01-07 12:46:53 -0500

    Static files

--- database/url.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import person_list
-
-urlpatterns = [
-  path('', person_list, name='person_list'),
-]
--- project_with_postgreSQL/settings.py
+++ project_with_postgreSQL/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ngtw*t%rxajhd-m#)ppm6uv=^z81jz9z037ze&6e*krs+%zx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'project_with_postgreSQL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'database', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,8 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
