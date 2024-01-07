47c3ac5cc78c9b318cf6ce3ce9d34f107eba9337
Yero-des <yero_zc@hotmail.com>
2024-01-07 11:15:16 -0500

    Corsheaders

--- project_with_postgreSQL/settings.py
+++ project_with_postgreSQL/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ngtw*t%rxajhd-m#)ppm6uv=^z81jz9z037ze&6e*krs+%zx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+CORS_ALLOWED_ORIGINS = [
+    "http://3.83.93.151/"
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -48,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'project_with_postgreSQL.urls'
@@ -77,9 +85,9 @@ WSGI_APPLICATION = 'project_with_postgreSQL.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'database_psql',
+        'NAME': 'people',
         'USER': 'postgres',
-        'PASSWORD': 'L/BUA7C2=juz',
+        'PASSWORD': 'Align2312',
         'HOST': 'localhost',  
         'PORT': '5432',   
     }
@@ -121,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
