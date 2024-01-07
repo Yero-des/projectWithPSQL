fc2371e6388ae5afc04493d6521ec5c102b980dd
Yero-des <yero_zc@hotmail.com>
2024-01-07 12:37:15 -0500

    First static

--- database/views.py
+++ database/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Person
+
+def person_list(request):
+  people = Person.objects.all()
+  context = {
+    "people": people,
+  }
+  return render(request, 'person_list.html', context=context)
--- project_with_postgreSQL/settings.py
+++ project_with_postgreSQL/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'project_with_postgreSQL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'database', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
